6b5255e846fc4af991c8be88db05d84d6929faf0
joanThompson123 <cross_cole@hotmail.com>
2022-02-16 20:45:14 -0300

    terceiro commit

---
+++ src/Teste.java
@@ -0,0 +1,34 @@
+
+public class Teste {
+	
+	int nota1;
+	int nota2;
+	
+	public Teste() {
+	}
+	
+	
+	public Teste(int nota1, int nota2) {
+		this.nota1 = nota1;
+		this.nota2 = nota2;
+	}
+	
+	
+	
+	public int getNota1() {
+		return nota1;
+	}
+	public void setNota1(int nota1) {
+		this.nota1 = nota1;
+	}
+	public int getNota2() {
+		return nota2;
+	}
+	public void setNota2(int nota2) {
+		this.nota2 = nota2;
+	}
+	
+	
+	
+
+}
